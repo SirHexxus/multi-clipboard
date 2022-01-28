99aa685ca01e2434ae9daa72c538b05d4579c997
SirHexxus <jamesmichaelstacy@gmail.com>
2022-01-28 15:19:14 -0800

    added shebang and def main if __name__ == __main__ for better use as a script

--- multiclipboard.py
+++ multiclipboard.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import clipboard
 import json
@@ -24,31 +26,36 @@ def list_data(filepath):
     print(data)
 
 
-if len(sys.argv) == 2:
-    command = sys.argv[1].lower()
-    data = load_data(SAVED_DATA)
-    if command == "save":
-        key = input("Enter a key: ")
-        data[key] = clipboard.paste()
-        save_data(SAVED_DATA, data)
-    elif command == "copy":
-        key = input("Enter a key: ")
-        if key in data:
-            clipboard.copy(data[key])
-            print("Copied to clipboard")
-        else:
-            print("Key not found")
-    elif command == "clear":
-        key = input("Enter a key: ")
-        if key in data:
-            del data[key]
+def main():
+    if len(sys.argv) == 2:
+        command = sys.argv[1].lower()
+        data = load_data(SAVED_DATA)
+        if command == "save":
+            key = input("Enter a key: ")
+            data[key] = clipboard.paste()
             save_data(SAVED_DATA, data)
-            print(f"Cleared {key}")
+        elif command == "copy":
+            key = input("Enter a key: ")
+            if key in data:
+                clipboard.copy(data[key])
+                print("Copied to clipboard")
+            else:
+                print("Key not found")
+        elif command == "clear":
+            key = input("Enter a key: ")
+            if key in data:
+                del data[key]
+                save_data(SAVED_DATA, data)
+                print(f"Cleared {key}")
+            else:
+                print("Key not found")
+        elif command == "list":
+            list_data(SAVED_DATA)
         else:
-            print("Key not found")
-    elif command == "list":
-        list_data(SAVED_DATA)
+            print('Invalid command')
     else:
-        print('Invalid command')
-else:
-    print('Invalid number of arguments')
+        print('Invalid number of arguments')
+
+
+if __name__ == "__main__":
+    main()
